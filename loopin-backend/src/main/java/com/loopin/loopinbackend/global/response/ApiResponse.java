02db4a7e2890b21f12be15e1175a3759c7a58a56
조성욱 <chosh0206@naver.com>
2025-05-13 15:39:08 +0900

    chore(global): 처음 공통 설정

---
+++ loopin-backend/src/main/java/com/loopin/loopinbackend/global/response/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.loopin.loopinbackend.global.response;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+
+    public ApiResponse(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+}
---
+++ loopin-backend/src/main/java/com/loopin/loopinbackend/global/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.loopin.loopinbackend.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
+
+
