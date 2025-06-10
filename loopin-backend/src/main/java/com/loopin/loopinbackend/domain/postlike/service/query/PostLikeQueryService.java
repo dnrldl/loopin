ec920657fe6e24a3716447d0bc0d4a2ea5f25283
조성욱 <chosh0206@naver.com>
2025-06-10 17:22:13 +0900

    feat: 게시글 좋아요 조회쿼리 제거

--- loopin-backend/src/main/java/com/loopin/loopinbackend/domain/postlike/service/query/PostLikeQueryService.java
+++
@@ -1,5 +0,0 @@
-package com.loopin.loopinbackend.domain.postlike.service.query;
-
-public interface PostLikeQueryService {
-
-}
--- loopin-backend/src/main/java/com/loopin/loopinbackend/domain/postlike/service/query/PostLikeQueryServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.loopin.loopinbackend.domain.postlike.service.query;
-
-public class PostLikeQueryServiceImpl {
-}
