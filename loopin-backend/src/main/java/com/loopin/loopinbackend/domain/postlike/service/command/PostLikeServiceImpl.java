7b5ae227e8289db5e5e27f5625346dcc9e2f2534
조성욱 <chosh0206@naver.com>
2025-06-10 17:22:13 +0900

    feat: 게시글 좋아요 조회쿼리 제거

--- loopin-backend/src/main/java/com/loopin/loopinbackend/domain/postlike/service/command/PostLikeServiceImpl.java
+++ loopin-backend/src/main/java/com/loopin/loopinbackend/domain/postlike/service/command/PostLikeServiceImpl.java
@@ -1,54 +1,54 @@
 package com.loopin.loopinbackend.domain.postlike.service.command;
 
+import com.loopin.loopinbackend.domain.post.repository.PostRepository;
 import com.loopin.loopinbackend.domain.postlike.entity.PostLike;
 import com.loopin.loopinbackend.domain.postlike.exception.AlreadyLikedException;
-import com.loopin.loopinbackend.domain.postlike.exception.NotLikedException;
 import com.loopin.loopinbackend.domain.postlike.exception.PostLikeNotFoundException;
 import com.loopin.loopinbackend.domain.postlike.repository.PostLikeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class PostLikeServiceImpl implements PostLikeService {
 
     private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void like(Long postId, Long userId) {
-        if (postLikeRepository.existsByPostIdAndUserId(postId, userId)) throw new PostLikeNotFoundException();
+        if (postLikeRepository.existsByPostIdAndUserId(postId, userId)) throw new AlreadyLikedException();
         String key = "post:" + postId + ":likes";
 
-        if (!redisTemplate.opsForSet().isMember(key, userId)) {
+        postLikeRepository.save(new PostLike(postId, userId));
+        postRepository.incrementLikeCount(postId);
+
+        try {
             redisTemplate.opsForSet().add(key, userId);
-        } else {
-            throw new AlreadyLikedException();
+        } catch (Exception e) {
+            log.warn("Redis 업데이트 실패: postId={}, userId={}", postId, userId);
         }
 
-        postLikeRepository.save(new PostLike(postId, userId));
     }
 
     @Override
     public void unlike(Long postId, Long userId) {
-        if (postLikeRepository.existsByPostIdAndUserId(postId, userId)) throw new PostLikeNotFoundException();
+        if (!postLikeRepository.existsByPostIdAndUserId(postId, userId)) throw new PostLikeNotFoundException();
         String key = "post:" + postId + ":likes";
-
-        if (redisTemplate.opsForSet().isMember(key, userId)) {
-            redisTemplate.opsForSet().remove(key, userId);
-        } else {
-            throw new NotLikedException();
-        }
 
         postLikeRepository.deleteByPostIdAndUserId(postId, userId);
-    }
+        postRepository.decrementLikeCount(postId);
 
-    @Override
-    public int countLike(Long postId) {
-        if (postLikeRepository.existsByPostId(postId)) throw new PostLikeNotFoundException();
-        String key = "post:" + postId + ":likes";
-
-        return redisTemplate.opsForSet().size(key).intValue();
+        try {
+            redisTemplate.opsForSet().remove(key, userId);
+        } catch (Exception e) {
+            log.warn("Redis 업데이트 실패: postId={}, userId={}", postId, userId);
+        }
     }
 }
